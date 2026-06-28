2096817a5fd39ffbb16467fb1605ee51fd6262be
li88iioo <laitunhuo@gmail.com>
2026-06-28 23:21:47 +0800

    fix: resolve VIP count staying at 0 by properly mapping Huya online message to vipCount and bump version to v1.8.13

--- simple_live_app/lib/modules/live_room/live_room_controller.dart
+++ simple_live_app/lib/modules/live_room/live_room_controller.dart
@@ -343,6 +343,9 @@ class LiveRoomController extends PlayerController with WidgetsBindingObserver {
       }
     } else if (msg.type == LiveMessageType.online) {
       online.value = msg.data;
+      if (site.id == "huya") {
+        vipCount.value = msg.data;
+      }
     } else if (msg.type == LiveMessageType.superChat) {
       superChats.add(msg.data);
     } else if (msg.type == LiveMessageType.gift) {
